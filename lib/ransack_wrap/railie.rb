a026d8c17871ddf02ddfe4b3774bb6479a2d2f8f
Tõnis Simo <anton.estum@gmail.com>
2013-08-21 01:03:00 +0400

    Fix

--- lib/ransack_wrap/railie.rb
+++
@@ -1,9 +0,0 @@
-require 'rails/railtie'
-
-module RansackWrap
-  class Railtie < Rails::Railtie
-    config.after_initialize do |app|
-      app.config.paths.add 'app/searchers', eager_load: true
-    end
-  end
-end
